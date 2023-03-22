231cf736b8e338eb01ff791035ea72f692fddf64
Andrés Peña Diaz <ca.penad@uniandes.edu.co>
2023-03-22 09:24:28 -0500

    Dockerfile and docker compose files edited to allow run PostgreSQL in a separate container

--- app/sophilabs_recruitment_process/settings.py
+++ app/sophilabs_recruitment_process/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "sophilabs_recruitment_process.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
         'NAME': 'sophilabs',
         'USER': 'postgres',
